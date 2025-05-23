8fedde0ebddb77772363b4be90f649eab9946a47
Vadim <vadim.gaidai@icloud.com>
2025-05-23 22:58:50 +0200

    fix: ky api url

--- src/lib/ky.ts
+++ src/lib/ky.ts
@@ -3,7 +3,7 @@ import ky, { type KyInstance } from 'ky'
 import { getAccessToken } from '@/utils/storage'
 
 const api: KyInstance = ky.create({
-  prefixUrl: '/api',
+  prefixUrl: `${import.meta.env.VITE_API_URL}/api/`,
   hooks: {
     beforeRequest: [
       (request: Request) => {
